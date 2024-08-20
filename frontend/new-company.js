0735a649d21d6e47dab21cd58a9ba0f4fd9d8ef6
Jack Vincent <162831739+vincent-jack@users.noreply.github.com>
2024-08-20 19:29:07 +0100

    add validation to new-company page

--- frontend/new-company.js
+++ frontend/new-company.js
@@ -2,26 +2,22 @@ const api_url = "https://api-b3hj.onrender.com"
 
 const submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', async function (e) {
-    const newCompanyName = document.getElementById('companyName').value
-    const newTown = document.getElementById('town').value
-    const newAddress = document.getElementById('address').value
-    const newTelephoneNumber = document.getElementById('telephoneNumber').value
-    const newWebsite = document.getElementById('website').value
-    const checkSave = document.getElementById('checkSave')
+    const newCompanyName = document.getElementById('companyName').value;
+    const newTown = document.getElementById('town').value;
+    const newAddress = document.getElementById('address').value;
+    const newTelephoneNumber = document.getElementById('telephoneNumber').value;
+    const newWebsite = document.getElementById('website').value;
+    const checkSave = document.getElementById('checkSave');
 
-    if (newCompanyName == "" || newTown == "" 
-        || newCompanyName.includes("'") == true || newCompanyName.includes('"') == true || newTown.includes("'") == true || newTown.includes('"') == true) {
-        const incorrectToastEl = document.getElementById('incorrectToast')
-        const incorrectToast = new bootstrap.Toast(incorrectToastEl)
-        incorrectToast.show();
+    if (validateForm(newCompanyName, newTown, newAddress, newTelephoneNumber, newWebsite) == false) {
         return
     }
 
-    document.getElementById('companyName').value = ''
-    document.getElementById('town').value = ''
-    document.getElementById('address').value = ''
-    document.getElementById('telephoneNumber').value = ''
-    document.getElementById('website').value = ''
+    document.getElementById('companyName').value = '';
+    document.getElementById('town').value = '';
+    document.getElementById('address').value = '';
+    document.getElementById('telephoneNumber').value = '';
+    document.getElementById('website').value = '';
 
     const data = JSON.stringify({
         company_name: newCompanyName,
@@ -40,8 +36,44 @@ submitButton.addEventListener('click', async function (e) {
         window.location.replace("companies.html");
     }
 
-    const successToastEl = document.getElementById('successToast')
-    const successToast = new bootstrap.Toast(successToastEl)
+    const successToastEl = document.getElementById('successToast');
+    const successToast = new bootstrap.Toast(successToastEl);
     successToast.show();
 
 });
+
+
+function validateForm(companyName, town, address, telephoneNumber, website) {
+    function isValidHttpUrl(string) {
+        let url;
+        try {
+          url = new URL(string);
+        } catch (_) {
+          return false;  
+        }
+        return true;
+    }
+
+    
+    let incorrectData = false;
+    const toastMessage = document.getElementById("incorrectToastMessage");
+    if (companyName == "" || town == "" || address == "" || telephoneNumber == "" || website == "") {
+        toastMessage.innerHTML = "Fields cannot be left blank.";
+        incorrectData = true;
+    } else if (/^\d+$/.test(telephoneNumber) == false) {
+        toastMessage.innerHTML = "Telephone Number field can only include numbers.";
+        incorrectData = true;
+    } else if (isValidHttpUrl(website) == false) {
+        toastMessage.innerHTML = "Website field must be a valid URL.";
+        incorrectData = true;
+    };
+
+    if (incorrectData == true) {
+        const incorrectToastEl = document.getElementById('incorrectToast');
+        const incorrectToast = new bootstrap.Toast(incorrectToastEl);
+        incorrectToast.show();
+        return false;
+    } else {
+        return true;
+    }
+};
