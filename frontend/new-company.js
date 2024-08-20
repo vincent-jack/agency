57368b456093e5a6d384ad46bc7bf5a8039cffc2
Jack Vincent <162831739+vincent-jack@users.noreply.github.com>
2024-08-20 17:39:42 +0100

    update new-company page and endpoint to support support new columns

--- frontend/new-company.js
+++ frontend/new-company.js
@@ -4,6 +4,9 @@ const submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', async function (e) {
     const newCompanyName = document.getElementById('companyName').value
     const newTown = document.getElementById('town').value
+    const newAddress = document.getElementById('address').value
+    const newTelephoneNumber = document.getElementById('telephoneNumber').value
+    const newWebsite = document.getElementById('website').value
     const checkSave = document.getElementById('checkSave')
 
     if (newCompanyName == "" || newTown == "" 
@@ -16,10 +19,16 @@ submitButton.addEventListener('click', async function (e) {
 
     document.getElementById('companyName').value = ''
     document.getElementById('town').value = ''
+    document.getElementById('address').value = ''
+    document.getElementById('telephoneNumber').value = ''
+    document.getElementById('website').value = ''
 
     const data = JSON.stringify({
         company_name: newCompanyName,
         town: newTown,
+        address: newAddress,
+        telephone_number: newTelephoneNumber,
+        website: newWebsite,
     });
 
     await fetch(api_url + '/companies/create', {
